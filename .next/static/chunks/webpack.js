43c0cc304a59a463afc1eb619ddaa2c3c1fd0eb7
김성욱 <sungwook@MacAir-2.local>
2023-03-16 22:58:26 +0900

    fix title

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4c802e64c40e5e3b"; }
+/******/ 		__webpack_require__.h = function() { return "1d8b71edeae57d8b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
