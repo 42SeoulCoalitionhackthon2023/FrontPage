4abac3e385afdfe03a2e236ab36cedf9368f4dc8
김성욱 <sungwook@MacAir-2.local>
2023-03-16 23:29:28 +0900

    title edit2

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,10 +4,10 @@ import { BsSearch } from "react-icons/bs";
 export default function Header() {
   return (
     <div className={styles.headerWrap}>
-      <div className={styles.title}>
+      <h1 className={styles.title}>
         15 Minutes <br />
         Is Enough!
-      </div>
+      </h1>
       <div className={styles.searchBar}>
         <BsSearch className={styles.searchIcon} />
         <input type="text" />
