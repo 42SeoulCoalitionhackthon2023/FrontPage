887ea25d3aee346b340fed3b9f7f004238175cb2
김성욱 <sungwook@MacAir-2.local>
2023-03-18 18:25:48 +0900

    ga 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import LoginChecker from "components/LoginChecker";
+import RouteChangeTracker from "../components/RouteChangeTracker";
+import ReactGA from "react-ga";
 
 export default function App({ Component, pageProps }) {
+  // Enable Google Analytics
+  RouteChangeTracker();
+
+  if (process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS) {
+    ReactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
+  }
+
   return (
-    <LoginChecker>
-      <Component {...pageProps} />
-    </LoginChecker>
+    <>
+      <LoginChecker>
+        <Component {...pageProps} />
+      </LoginChecker>
+    </>
   );
 }
