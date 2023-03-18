9b21aa4654a54df09580cb42571b89247e819414
김성욱 <sungwook@MacAir-2.local>
2023-03-18 17:24:52 +0900

    코드 입력 구현

---
+++ components/LoginChecker.tsx
@@ -0,0 +1,45 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import styles from "../styles/components/loginchecker.module.scss";
+
+interface LoginCheckerProps {
+  children: React.ReactNode;
+}
+
+export default function LoginChecker({ children }: LoginCheckerProps) {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [logginKey, setLogginKey] = useState<string>("");
+
+  const router = useRouter();
+
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const keyword = e.target.value;
+    setLogginKey(keyword);
+  };
+
+  useEffect(() => {
+    if (logginKey === "평가풍년") {
+      localStorage.setItem("15ME-token", logginKey);
+      router.replace(`/`);
+    }
+    if (localStorage.getItem("15ME-token")) {
+      setLoggedIn(true);
+    }
+  }, [logginKey]);
+
+  return loggedIn ? (
+    <>{children}</>
+  ) : (
+    <div className={styles.pageWrap}>
+      <div className={styles.background}>
+        <div className={styles.inputWrap}>
+          <div className={styles.text}>코드를 입력해주세요!</div>
+          <input
+            value={logginKey}
+            onChange={inputHandler}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,9 @@
+import LoginChecker from "components/LoginChecker";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <LoginChecker>
+      <Component {...pageProps} />
+    </LoginChecker>
+  );
 }
