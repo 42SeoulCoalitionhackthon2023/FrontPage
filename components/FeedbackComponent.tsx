a16b9a86edf74188a7780a2257debde7fb4d97fd
SeungWoonSong <susong@student.42seoul.kr>
2023-03-18 20:34:51 +0900

    fix : dependecy infin loop solve

--- components/FeedbackComponent.tsx
+++ components/FeedbackComponent.tsx
@@ -133,7 +133,7 @@ export default function FeedbackComponent({ userId }: { userId: number }) {
     } else {
       setSubjects(circleSix);
     }
-  }, [circleHandler, subjectHandler]);
+  }, []);
 
   useEffect(() => {
     if (subjectBtn) {
