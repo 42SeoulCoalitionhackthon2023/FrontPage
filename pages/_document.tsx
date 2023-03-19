e6d6771f905489a98725df054648522fd94dd9f3
김성욱 <sungwook@MacAir-2.local>
2023-03-19 17:36:00 +0900

    fix : user stylesheet margin 0

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <title>15 Minutes Is Enough!</title>
-      <body>
+      <body style={{ margin: 0 }}>
         <Main />
         <NextScript />
       </body>
